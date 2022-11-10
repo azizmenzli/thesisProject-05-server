29dcae1738948b5c06d6203a72774d98fda46467
azizmenzli <azizmenzli3@gmail.com>
2022-11-10 16:21:27 +0100

    work is one

---
+++ server.js
@@ -0,0 +1,20 @@
+const   express=require('express');
+const app=express()
+const cors =require('cors')
+const connection= require('./database/index')
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+
+let port =3001;
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+    }
+console.log(`server is listening on port ${port}`);
+})
